2866070573f1a5f916f01d50736d899c993d49f2
lukaszkuzmicki <kuzmickilu@gmail.com>
2019-11-04 19:47:12 +0100

    test

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit {
   }
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    this.registerMode = true;
   }
 
   getValues() {
@@ -28,6 +28,9 @@ export class HomeComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+  cancelRegisterMode(registerMode: boolean){
+    this.registerMode = registerMode;
   }
 
 }
