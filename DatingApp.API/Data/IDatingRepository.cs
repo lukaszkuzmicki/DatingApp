3031f54f95591c7e0ff8253315ca6e570d8b642c
lukaszkuzmicki <kuzmickilu@gmail.com>
2019-11-15 20:29:24 +0100

    AutoMapper added and some dtos

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -10,7 +10,7 @@ namespace DatingApp.API.Data
          void Add<T>(T entity) where T: class;
          void Delete<T>(T entity) where T: class;
          Task<bool> SaveAll();
-         //Task<IEnumerable<User>> GetUsers();
+         Task<IEnumerable<User>> GetUsers();
          Task<User> GetUser(int id);
     }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -50,6 +50,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             //services.AddConnections();
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
             
 
             
