98b1f4234a43168b3a7ff5b76767e4505101948b
lukaszkuzmicki <kuzmickilu@gmail.com>
2020-02-01 15:56:21 +0100

    cloudinary implemented

--- DatingApp.API/Dtos/PhotoForReturnDto.cs
+++ DatingApp.API/Dtos/PhotoForReturnDto.cs
@@ -2,13 +2,13 @@ using System;
 
 namespace DatingApp.API.Dtos
 {
-    public class PhotoForDetailedDto
+    public class PhotoForReturnDto
     {
         public int Id { get; set; }
         public string Url { get; set; }
         public string description { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }        
-        public int UserId { get; set; }
+        public string Public { get; set; }
     }
 }
